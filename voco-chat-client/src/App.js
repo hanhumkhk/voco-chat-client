0562eaacc72e8eb0251a98a8808b734a48521a7a
hanna <hanna.hummal@khk.ee>
2022-03-20 21:05:49 +0200

    muudatused,react bootstrap, laiendidjne

--- voco-chat-client/src/App.js
+++ voco-chat-client/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import '../src/css/App.css';
+//import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import DUMMY_DATA from './data';
 
-function App() {
+function MessageRow(message, index) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-box" key={index}>
+      <div className="message" >{message.text}</div>
+      <div className="user-name" >{message.senderId}</div>
     </div>
+  )
+}
+
+function Chat() {
+  
+  return (
+    <Container className="page-container">
+      <h1>VOCO</h1>
+      <div className="chat-container">
+        <div className="messages-box">
+          {DUMMY_DATA.map((message, index) => MessageRow(message, index))}
+        </div>
+
+        <div className="user-box">
+          <div className="user"><b>Guest</b></div>
+          <div className="user-message">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</div>
+        </div>
+        <Button >Send</Button>
+      </div>
+
+    </Container>
   );
 }
 
-export default App;
+export default Chat;
